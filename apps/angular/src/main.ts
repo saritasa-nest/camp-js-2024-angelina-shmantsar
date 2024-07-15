982709a90e035a69c0e3e478218cc41255326580
Angelina Shmantsar <angelina.shmantsar@saritasa.com>
2024-07-15 16:15:08 +0500

    Add Angular Material JC-643

--- apps/angular/src/main.ts
+++ apps/angular/src/main.ts
@@ -2,14 +2,17 @@ import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { appRoutes } from './app/app.routes';
+
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
-import { appRoutes } from './app/app.routes';
 
 if (environment.production) {
 	enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-	providers: [provideRouter(appRoutes)],
+	providers: [provideRouter(appRoutes), provideAnimationsAsync()],
 }).catch(err => console.error(err));
