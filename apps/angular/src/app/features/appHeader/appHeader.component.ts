b1c21860c44a3a43dd19f76a61a275a1234141bc
Angelina Shmantsar <angelina.shmantsar@saritasa.com>
2024-07-17 09:40:37 +0500

    Change app header content and styles JC-643

--- apps/angular/src/app/features/appHeader/appHeader.component.ts
+++ apps/angular/src/app/features/appHeader/appHeader.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 /** Represents app header. */
 @Component({
 	selector: 'app-header',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, MatIconModule, MatButtonModule],
 	templateUrl: './appHeader.component.html',
 	styleUrls: ['./appHeader.component.css'],
 })
