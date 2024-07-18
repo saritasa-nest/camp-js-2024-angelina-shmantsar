cb035f8cd698ed0b59843e08bcfbb53f9c7f4a14
Angelina Shmantsar <angelina.shmantsar@saritasa.com>
2024-07-18 10:06:52 +0500

    Create url service JC-643

--- apps/angular/src/core/services/anime.service.ts
+++ apps/angular/src/core/services/anime.service.ts
@@ -2,13 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable, map } from 'rxjs';
 
-import { BASE_API_URL } from '../constants/backendConst';
-
 import { GetAnimeResponseDto } from '../dtos/getAnimeResponse.dto';
 import { AnimeDto } from '../dtos/anime.dto';
 import { AnimeMapper } from '../mappers/anime.mapper';
 import { Anime } from '../models/anime';
 import { convertIsoToLocale } from '../utils/convertIsoToLocale';
+
+import { UrlService } from './url.service';
 
 /** Represents anime fetch and transform service. */
 @Injectable({
@@ -17,13 +17,15 @@ import { convertIsoToLocale } from '../utils/convertIsoToLocale';
 export class AnimeService {
 	private httpClient = inject(HttpClient);
 
-	private baseAnimeUrl = `${BASE_API_URL}anime/`;
+	private urlService = inject(UrlService);
+
+	private animeUrl = this.urlService.getAnimeUrl();
 
 	public constructor() {}
 
 	/** Fetch all anime from backend. */
 	public getAllAnime(): Observable<Anime[]> {
-		return this.httpClient.get<GetAnimeResponseDto>(`${this.baseAnimeUrl}anime/`, {
+		return this.httpClient.get<GetAnimeResponseDto>(this.animeUrl, {
 			headers: {
 				// Json specific naming
 				// eslint-disable-next-line @typescript-eslint/naming-convention
---
+++ apps/angular/src/core/services/url.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+import { BASE_API_URL } from '../constants/backendConst';
+
+/** Creates urls. */
+@Injectable({
+	providedIn: 'root',
+})
+export class UrlService {
+	private baseAnimeUrl = `${BASE_API_URL}anime/`;
+
+	/** Get anime endpoint url. */
+	public getAnimeUrl(): string {
+		return `${this.baseAnimeUrl}anime/`;
+	}
+}
