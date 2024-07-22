d51f15685b663d10d6ddfa5a04ceedd27062172a
Angelina Shmantsar <angelina.shmantsar@saritasa.com>
2024-07-22 10:29:01 +0500

    Reorganized url in url service JC-643

--- apps/angular/src/core/services/api-url.service.ts
+++ apps/angular/src/core/services/api-url.service.ts
@@ -2,14 +2,17 @@ import { Injectable } from '@angular/core';
 
 import { environment } from '@js-camp/angular/environments/environment';
 
+/** Endpoints. */
+enum Endpoints {
+	Anime = 'anime/anime/',
+}
+
+const { baseApiUrl } = environment;
+
 /** Creates urls. */
 @Injectable({ providedIn: 'root' })
 export class ApiUrlService {
-	private readonly baseApiUrl = environment.baseApiUrl;
-
-	private readonly baseAnimeUrl = `${this.baseApiUrl}anime/`;
-
-	private readonly _allAnimeUrl = `${this.baseAnimeUrl}anime/`;
+	private readonly _allAnimeUrl = `${baseApiUrl}${Endpoints.Anime}`;
 
 	/** Get anime endpoint url. */
 	public get allAnimeUrl(): string {
