4339246c67cc6173d26cbc4cf3db218ef384aad0
Angelina Shmantsar <angelina.shmantsar@saritasa.com>
2024-08-05 09:36:09 +0500

    Add lazy loading JC-672

--- apps/angular/src/app/app.routes.ts
+++ apps/angular/src/app/app.routes.ts
@@ -1,19 +1,21 @@
 import { Routes } from '@angular/router';
 
-import { AppHeaderComponent } from '../shared/components/app-header/app-header.component';
-
-import { AnimeTableComponent } from './features/anime-table/anime-table.component';
-import { AuthComponent } from './features/auth/auth.component';
-
 /** Routes object. */
 export const appRoutes: Routes = [
 	{
 		path: '',
-		component: AppHeaderComponent,
-		children: [{ path: '', component: AnimeTableComponent }],
+		loadComponent: () =>
+			import('../shared/components/app-header/app-header.component').then(value => value.AppHeaderComponent),
+		children: [
+			{
+				path: '',
+				loadComponent: () =>
+					import('./features/anime-table/anime-table.component').then(value => value.AnimeTableComponent),
+			},
+		],
 	},
 	{
 		path: 'auth',
-		component: AuthComponent,
+		loadComponent: () => import('./features/auth/auth.component').then(value => value.AuthComponent),
 	},
 ];
