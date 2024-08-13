aef5afad97f097589b73fb07618a52eaed2c5d0a
Angelina Shmantsar <angelina.shmantsar@saritasa.com>
2024-08-13 09:38:23 +0500

    Add setter for error message JC-672

--- apps/angular/src/app/features/auth/components/error/error.component.ts
+++ apps/angular/src/app/features/auth/components/error/error.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { BehaviorSubject } from 'rxjs';
@@ -12,16 +12,13 @@ import { BehaviorSubject } from 'rxjs';
 	styleUrl: './error.component.css',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ErrorComponent implements OnChanges {
+export class ErrorComponent {
 	/** Error message. */
 	@Input()
-	public errorMessage = '';
+	public set errorMessage(value: string) {
+		this.errorMessage$.next(value);
+	}
 
 	/** Error message. */
 	protected readonly errorMessage$ = new BehaviorSubject('');
-
-	/** @inheritdoc */
-	public ngOnChanges(changes: SimpleChanges): void {
-		this.errorMessage$.next(changes['errorMessage'].currentValue);
-	}
 }
