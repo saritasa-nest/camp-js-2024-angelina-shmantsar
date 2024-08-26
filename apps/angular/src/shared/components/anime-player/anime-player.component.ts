1c9bc970339dbc4111987e0df4bfb49634bab995
Angelina Shmantsar <angelina.shmantsar@saritasa.com>
2024-08-26 17:42:29 +0500

    Try to fix SonarCloud error JC-685

--- apps/angular/src/shared/components/anime-player/anime-player.component.ts
+++ apps/angular/src/shared/components/anime-player/anime-player.component.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, inject } from '@angular/core';
 import { AsyncPipe, CommonModule } from '@angular/common';
-import { SafePipe } from '@js-camp/angular/core/pipes/safe.pipe';
 import { BehaviorSubject } from 'rxjs';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 import { YOUTUBE_BASE_URL } from '../../constants/youtube-base-url';
 
@@ -11,7 +11,7 @@ import { YOUTUBE_BASE_URL } from '../../constants/youtube-base-url';
 @Component({
 	selector: 'camp-anime-player',
 	standalone: true,
-	imports: [CommonModule, SafePipe, AsyncPipe, MatCardModule, MatIconModule],
+	imports: [CommonModule, AsyncPipe, MatCardModule, MatIconModule],
 	templateUrl: './anime-player.component.html',
 	styleUrl: './anime-player.component.css',
 	changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,6 +22,9 @@ export class AnimePlayerComponent {
 	public set trailerYouTubeId(id: string) {
 		this.trailerYouTubeUrl$.next(`${this.youtubeBaseUrl}${id}`);
 	}
+
+	/** Sanitizer. */
+	protected readonly sanitizer = inject(DomSanitizer);
 
 	/** Anime trailer youtube url. */
 	protected readonly trailerYouTubeUrl$ = new BehaviorSubject('');
